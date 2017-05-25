f0b475133a333e8c59cca8a1abbca2e8f2e8e697
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-05-25 14:43:59 -0400

    csp

---
+++ js/kyle.js
@@ -0,0 +1,17 @@
+document.getElementById("year").text = new Date().getFullYear();
+
+if(location.search === "?emailed=true") {
+    document.getElementById("email-success").style.display = "block";
+    
+    if (history.replaceState) {
+        history.replaceState({}, '', location.pathname);
+    }
+}
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-1080667-3', 'auto');
+    ga('send', 'pageview');
