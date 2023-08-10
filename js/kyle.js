ce68e75b58bcdc49148f2c1081a484f08f8da246
Kyle Spearrin <kspearrin@users.noreply.github.com>
2023-08-10 16:50:20 -0400

    Update kyle.js

--- js/kyle.js
+++ js/kyle.js
@@ -8,6 +8,35 @@ if(location.search === "?emailed=true") {
     }
 }
 
+var form = document.getElementById("contact-form");
+async function handleSubmit(event) {
+  event.preventDefault();
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      "Accept": "application/json",
+    }
+  }).then(response => {
+    if (response.ok) {
+      document.getElementById("email-success").style.display = "block";
+      form.reset();
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, "errors")) {
+          alert(data["errors"].map(error => error["message"]).join(", "));
+        } else {
+          alert("Oops! There was a problem submitting your form.");
+        }
+      })
+    }
+  }).catch(error => {
+    alert("Oops! There was a problem submitting your form.");
+  });
+}
+form.addEventListener("submit", handleSubmit);
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
